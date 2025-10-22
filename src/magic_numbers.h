9a44fc1dc8963c057a123bd816f64c184f9b8e00
Sébastien Nouira Huet <148809822+fafa64no@users.noreply.github.com>
2025-10-22 16:13:49 +0200

    [UPDATE] magic_numbers.h has all necessary paths

--- src/magic_numbers.h
+++ src/magic_numbers.h
@@ -11,4 +11,13 @@
 #define DEFAULT_DIV_INPUT_PATH "../data/inpdivseq.txt"
 #define DEFAULT_DIV_OUTPUT_PATH "../data/outdivseq.txt"
 
+#define DEFAULT_ARB_INPUT_PATH "../data/inparbgraph.txt"
+#define DEFAULT_ARB_OUTPUT_PATH "../data/outarbgraph.txt"
+
+#define DEFAULT_CON_INPUT_PATH "../data/inpcongraph.txt"
+#define DEFAULT_CON_OUTPUT_PATH "../data/outcongraph.txt"
+
+#define DEFAULT_DIJ_INPUT_PATH "../data/inpdijgraph.txt"
+#define DEFAULT_DIJ_OUTPUT_PATH "../data/outdijgraph.txt"
+
 #endif //ENSEA_ALGO_TP2_MAGIC_NUMBERS_H
